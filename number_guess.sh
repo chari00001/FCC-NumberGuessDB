40e548ebcadccab60da871b865c87281d10700ee
chari00001 <cagrilacin303@gmail.com>
2023-07-03 12:54:30 +0000

    test: some tests made

--- number_guess.sh
+++ number_guess.sh
@@ -3,35 +3,41 @@ PSQL="psql --username=freecodecamp --dbname=users -t --no-align -c"
 
 RANDOM_NUM=$((RANDOM % 1001))
 
+echo  "$RANDOM_NUM"
 echo "Enter your username:"
 read USERNAME
 
 USER_EXIST=$($PSQL "SELECT * FROM users WHERE username='$USERNAME';")
-USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME';")
 NAME=$($PSQL "SELECT username FROM users WHERE username='$USERNAME';")
-GAMES=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID;")
-BEST=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USER_ID;")
-
 
 if [[ -z $USER_EXIST ]]
   then
-    echo "Welcome, $NAME! It looks like this is your first time here."
+    echo "Welcome, $USERNAME! It looks like this is your first time here."
     INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME');")
+    USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME';")
   else
     echo "Welcome back, $NAME! You have played $GAMES games, and your best game took $BEST guesses."
+    INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$NAME');")
+    if [[ $INSERT_USER_RESULT = "INSERT 0 1" ]]
+      then 
+        USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME';")
+    fi
 fi
+
+GAMES=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID;")
+BEST=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USER_ID;")
 
 FLAG_CHECK=true
 TOTAL_GUESSES=1
 
-while $FLAG_CHECK = true
+while $FLAG_CHECK
 do
   echo "Guess the secret number between 1 and 1000:"
   read GUESS
-  if [[ GUESS = $RANDOM_NUM ]]
+  if [[ $GUESS = $RANDOM_NUM ]]
     then
       echo "You guessed it in $TOTAL_GUESSES tries. The secret number was $RANDOM_NUM. Nice job!"
-      FLAG=false
+      FLAG_CHECK=false
     else
       ((TOTAL_GUESSES++))
   fi
