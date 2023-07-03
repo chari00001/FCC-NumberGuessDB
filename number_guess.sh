17c93a9238ed7b4991d6485fe220cfea5283abf5
chari00001 <cagrilacin303@gmail.com>
2023-07-03 12:23:19 +0000

    fix: game vals made dynamic

--- number_guess.sh
+++ number_guess.sh
@@ -7,9 +7,10 @@ echo "Enter your username:"
 read USERNAME
 
 USER_EXIST=$($PSQL "SELECT * FROM users WHERE username='$USERNAME';")
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME';")
 NAME=$($PSQL "SELECT username FROM users WHERE username='$USERNAME';")
-GAMES=$($PSQL "SELECT games FROM users WHERE username='$USERNAME';")
-BEST=$($PSQL "SELECT best_game FROM users WHERE username='$USERNAME';")
+GAMES=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID;")
+BEST=$($PSQL "SELECT MIN(guesses) FROM games WHERE user_id=$USER_ID;")
 
 
 if [[ -z $USER_EXIST ]]
